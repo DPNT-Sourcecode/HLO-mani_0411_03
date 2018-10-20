f22807463ad3aaa56bb62162555c34f8a24e46f0
sbx_user1060 <sbx_user1060@ip-10-128-64-128.eu-west-2.compute.internal>
2018-10-20 23:06:42 +0000

    Sat Oct 20 23:06:42 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -59,6 +59,12 @@ public class FizzBuzzSolutionTest {
         assertThat(fizzBuzz.fizzBuzz(45), equalTo("fizz buzz"));
         assertThat(fizzBuzz.fizzBuzz(30), equalTo("fizz buzz"));
         assertThat(fizzBuzz.fizzBuzz(600), equalTo("fizz buzz"));
+
+        //- A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
+        //- A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
+        //- We should still say "fake deluxe" if the "deluxe" number is odd
+        //      - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
+
     }
 }
 
