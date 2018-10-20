5d42bbb1d963fb7051a99a0965ceda15e3b5a246
sbx_user1059 <sbx_user1059@ip-10-128-64-128.eu-west-2.compute.internal>
2018-10-20 21:25:33 +0000

    Sat Oct 20 21:25:33 UTC 2018

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,23 @@
+package befaster.solutions.HLO;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HelloSolutionTest {
+    private SumSolution sum;
+
+    @Before
+    public void setUp() {
+
+        sum = new SumSolution();
+    }
+
+    @Test
+    public void compute_sum() {
+        assertThat(sum.compute(1, 1), equalTo(2));
+    }
+}
