7e4943e942148e0805356bc31c5f842a59c1f5a9
sbx_user1059 <sbx_user1059@ip-10-128-64-128.eu-west-2.compute.internal>
2018-10-20 22:22:40 +0000

    Sat Oct 20 22:22:40 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,7 +4,7 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
         String numberAsString = Integer.toString(number);
-        String result;
+        String result = "";
         if ((number % 15 == 0) ||
                 containsDigitsDivisbleBy3Or5(numberAsString)) {
             result = "fizz buzz";
@@ -14,14 +14,28 @@ public class FizzBuzzSolution {
         } else if ((number % 3 == 0) ||
                 numberAsString.contains("3")) {
             result = "fizz";
+        }
+
+        if ((number > 10) && allDigitsAreIdentical(numberAsString)) {
+            result = result + " deluxe";
         } else {
             result = Integer.toString(number);
         }
 
-        if ((number > 10) && allDigitsAreIdentical(numberAsString) {
-            result = result + " deluxe";
-        }
         return result;
+    }
+
+    private boolean allDigitsAreIdentical(String numberAsString) {
+        char[] charArray = numberAsString.toCharArray();
+        for (int index = 1; index < charArray.length; index++) {
+            char previousDigitAsChar = charArray[index - 1];
+            char thisDigitAsChar = charArray[index];
+
+            if (previousDigitAsChar != thisDigitAsChar) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean containsDigitsDivisbleBy3Or5(String number) {
@@ -29,7 +43,7 @@ public class FizzBuzzSolution {
         boolean divisibleBy5 = false;
 
         String numberAsString = number.replace("0", "");
-        for (char digitAsChar: numberAsString.toCharArray()) {
+        for (char digitAsChar : numberAsString.toCharArray()) {
             int digit = Integer.parseInt(String.valueOf(digitAsChar));
             if (digit % 3 == 0) {
                 divisibleBy3 = true;
