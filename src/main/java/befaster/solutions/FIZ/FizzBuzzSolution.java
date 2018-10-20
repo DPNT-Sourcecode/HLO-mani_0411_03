0eee110d07edda8661e1457f69b3f2feeeed785c
sbx_user1059 <sbx_user1059@ip-10-128-64-128.eu-west-2.compute.internal>
2018-10-20 21:47:35 +0000

    Sat Oct 20 21:47:35 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,11 +4,14 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
         String numberAsString = Integer.toString(number);
-        if ((number % 15 == 0) || (numberAsString.contains("3") && numberAsString.contains("5"))) {
+        if ((number % 15 == 0) ||
+                (numberAsString.contains("3") && numberAsString.contains("5"))) {
             return "fizz buzz";
-        } else if ((number % 5 == 0) || numberAsString.contains("5")) {
+        } else if ((number % 5 == 0) ||
+                numberAsString.contains("5")) {
             return "buzz";
-        } else if ((number % 3 == 0) || numberAsString.contains("3")) {
+        } else if ((number % 3 == 0) ||
+                numberAsString.contains("3")) {
             return "fizz";
         }
         return Integer.toString(number);
