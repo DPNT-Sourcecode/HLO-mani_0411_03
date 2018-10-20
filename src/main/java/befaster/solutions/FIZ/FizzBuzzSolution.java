d0d25bffd344bbe1ce8d70713df74ebf9b1db308
sbx_user1059 <sbx_user1059@ip-10-128-64-128.eu-west-2.compute.internal>
2018-10-20 21:59:57 +0000

    Sat Oct 20 21:59:57 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,7 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         String numberAsString = Integer.toString(number);
         if ((number % 15 == 0) ||
-                (numberAsString.contains("3") && numberAsString.contains("5"))) {
+                containsDigitsDivisbleBy3Or5(numberAsString)) {
             return "fizz buzz";
         } else if ((number % 5 == 0) ||
                 numberAsString.contains("5")) {
@@ -15,6 +15,22 @@ public class FizzBuzzSolution {
             return "fizz";
         }
         return Integer.toString(number);
+    }
+
+    private boolean containsDigitsDivisbleBy3Or5(String number) {
+        boolean divisibleBy3 = false;
+        boolean divisibleBy5 = false;
+        for (char digitAsChar: number.toCharArray()) {
+            int digit = (int)digitAsChar;
+            if (digit % 3 == 0) {
+                divisibleBy3 = true;
+            }
+
+            if (digit % 5 == 0) {
+                divisibleBy5 = true;
+            }
+        }
+        return divisibleBy3 && divisibleBy5;
     }
 
 }
