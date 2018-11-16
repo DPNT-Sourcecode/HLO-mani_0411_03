049994aebb5818644f960be87d1fbd72be385019
sbx_user1059 <sbx_user1059@ip-10-129-119-253.eu-west-2.compute.internal>
2018-11-16 00:17:11 +0000

    Fri Nov 16 00:17:11 UTC 2018

--- src/main/java/befaster/runner/Utils.java
+++ src/main/java/befaster/runner/Utils.java
@@ -15,8 +15,7 @@ public class Utils {
 
     public static ImplementationRunnerConfig getRunnerConfig() throws ConfigNotFoundException {
         return new ImplementationRunnerConfig()
-                .setRequestQueueName(readFromConfigFile("tdl_request_queue_name"))
-                .setResponseQueueName(readFromConfigFile("tdl_response_queue_name"))
+                .setUniqueId(readFromConfigFile("tdl_username"))
                 .setHostname(readFromConfigFile("tdl_hostname"));
     }
 }
